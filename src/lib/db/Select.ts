ca1a4a4eef721f698a969e20499b577a28179c30
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-06-06 20:51:52 -0600

    added more Selects for prisma responses

--- src/lib/db/Select.ts
+++ src/lib/db/Select.ts
@@ -1,9 +1,20 @@
 import { Prisma } from '@prisma/client';
 
+export const sessionSelect: Prisma.SessionSelect = {
+  id: true,
+  expiresAt: true,
+};
+
 export const accountSelect: Prisma.AccountSelect = {
   id: true,
   firstName: true,
   lastName: true,
   email: true,
   dateOfBirth: true,
+};
+
+export const accountAuthSelect: Prisma.AccountSelect = {
+  id: true,
+  email: true,
+  password: true,
 };
