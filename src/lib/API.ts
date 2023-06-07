27ba0a29a5d69ccbd5b91d99a6a6b37a6085cc8f
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-06-06 20:50:13 -0600

    added AnyObj over any type

--- src/lib/API.ts
+++ src/lib/API.ts
@@ -1,7 +1,9 @@
+import type { AnyObj } from '@/types';
+
 type APIResponseStatus = 'success' | 'error' | 'warning' | 'info';
 
 export class API {
-  static res(authorized: boolean, status: APIResponseStatus, data: any, message: string) {
+  static res(authorized: boolean, status: APIResponseStatus, data: AnyObj, message: string) {
     return { authorized, status, data, message };
   }
 }
