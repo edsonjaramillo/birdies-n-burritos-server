ecbffe9ca206e912ec24500158ecad68ef09d037
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-06-07 09:24:30 -0600

    added explicit type to jwt verify

--- src/utils/JWT.ts
+++ src/utils/JWT.ts
@@ -1,12 +1,18 @@
 import jwt from 'jsonwebtoken';
+
 import type { AnyObj } from '@/types';
+
+type JWTVerifyResult = {
+  status: boolean;
+  payload: jwt.JwtPayload | null;
+};
 
 export class JWT {
   static sign(payload: AnyObj, secret: string, expiresIn: number) {
     return jwt.sign(payload, secret, { algorithm: 'HS256', expiresIn });
   }
 
-  static verify(token: string, secret: string) {
+  static verify(token: string, secret: string): JWTVerifyResult {
     let status = true;
     let payload = null;
     try {
