1d876f96f4ea952d9717b72002e657863c83d0da
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-06-06 20:55:38 -0600

    added jwt and zod middleware

--- src/routes/account.ts
+++ src/routes/account.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import accountsController from '@/controllers/account';
 import { getAccountValidator, createAccountValidator } from '@/validators/account';
-import { validate } from '@/middleware';
+import { zodValidate } from '@/middleware/zodValidate';
+import jwtVerify from '@/middleware/jwtVerify';
 
 const router = express.Router();
 
-router.route('/get-account').post(validate(getAccountValidator), accountsController.getAccount);
-router.route('/create-account').post(validate(createAccountValidator), accountsController.createAccount);
+router.route('/get-account').post(jwtVerify(), zodValidate(getAccountValidator), accountsController.getAccount);
+router.route('/create-account').post(zodValidate(createAccountValidator), accountsController.createAccount);
 
 export default router;
