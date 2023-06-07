4d0490298d43fed73a5fc9a9bb4290a3783c4d39
Edson Jaramillo <edsonajaramillo@gmail.com>
2023-06-06 20:51:18 -0600

    reversed order for readability

--- src/utils/Security.ts
+++ src/utils/Security.ts
@@ -5,7 +5,7 @@ export class Security {
     return argon2.hash(password, { type: argon2.argon2id });
   }
 
-  static async verifyPassword(password: string, hash: string): Promise<boolean> {
-    return argon2.verify(hash, password, { type: argon2.argon2id });
+  static async verifyPassword(hash: string, plain: string): Promise<boolean> {
+    return argon2.verify(hash, plain, { type: argon2.argon2id });
   }
 }
